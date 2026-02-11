8ec8905a067420f36045e61366f554fa3ef03e61
imad-dahman <2010imdahman@student.epfc.eu>
2026-02-11 11:20:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',                 // ← important pour GitHub Pages (site perso)
+  base: '/',                 // ← important pour GitHub Pages (site perso)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
