9bb575a1441f7f5b673413016aa7830d96228410
imad-dahman <2010imdahman@student.epfc.eu>
2026-02-11 11:14:04 +0100

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -56,13 +56,13 @@ function Sidebar() {
           />
 
           <h1 className="profile-name">Imad Dahman</h1>
-          <p className="profile-subtitle">Junior Full-Stack Developer </p>
+          <p className="profile-subtitle"> Junior Full-Stack Developer </p>
 
           <p className="profile-bio">
            Développeur Full-Stack diplômé en Informatique de Gestion (EPFC, Bruxelles).
-Spécialisé en développement d’applications web modernes avec React, Node.js, PHP et MySQL.
-Expérience en développement d’API REST, systèmes d’authentification (OAuth) et optimisation SEO.
-Je conçois des solutions performantes, sécurisées et orientées utilisateur.
+          Spécialisé en développement d’applications web modernes avec React, Node.js, PHP et MySQL.
+          Expérience en développement d’API REST, systèmes d’authentification (OAuth) et optimisation SEO.
+          Je conçois des solutions performantes, sécurisées et orientées utilisateur.
           </p>
 
           <div className="social-links">
